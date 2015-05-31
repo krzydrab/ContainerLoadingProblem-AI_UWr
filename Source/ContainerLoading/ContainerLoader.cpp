78dc5c84d53526e6a7e531abf22b562db4e7739d
Krabex <krabex93@gmail.com>
2015-05-31 11:54:32 +0200

    Added ContainerLoader class

---
+++ Source/ContainerLoading/ContainerLoader.cpp
@@ -0,0 +1,43 @@
+#include "ContainerLoader.h"
+
+namespace ContainerLoading
+{
+    ContainerLoader::ContainerLoader(const Utils::Container container, IPackageGenerator* packageGenerator)
+        : _container(container)
+    {
+        _packages = packageGenerator->generatePackages();
+        _algorithm = nullptr;
+    }
+
+    ContainerLoader::~ContainerLoader()
+    {
+        if(_algorithm != nullptr)
+        {
+            delete _algorithm;
+        }
+    }
+    
+    bool ContainerLoader::run(int steps)
+    {
+        if(_algorithm != nullptr)
+        {
+            return _algorithm->run(steps);
+        }
+        return false;
+    }
+
+    bool ContainerLoader::singleStep()
+    {
+        return run(1);
+    }
+
+    Utils::Container& ContainerLoader::getContainer()
+    {
+        return _container;
+    }
+
+    std::vector<Utils::Package>& ContainerLoader::getPackages()
+    {
+        return _packages;
+    }
+}
---
+++ Source/ContainerLoading/ContainerLoader.h
@@ -0,0 +1,29 @@
+#ifndef CONTAINERLOADING_CONTAINERLOADER
+#define CONTAINERLOADING_CONTAINERLOADER
+
+#include "Algorithm/IAlgorithm.h"
+#include "PackageGenerator.h"
+
+namespace ContainerLoading
+{
+    class ContainerLoader
+    {
+    public:
+        ContainerLoader(const Utils::Container container, IPackageGenerator* packageGenerator);
+        ~ContainerLoader();
+
+        Utils::Container& getContainer();
+        std::vector<Utils::Package>& getPackages();
+        void applyAlgorithm(Algorithm::IAlgorithm* algorithm);
+
+        bool run(int steps);
+        bool singleStep();
+
+    private:
+        Utils::Container _container;
+        std::vector<Utils::Package> _packages;
+        Algorithm::IAlgorithm* _algorithm;
+    };
+}
+
+#endif /* CONTAINERLOADING_CONTAINERLOADER */
