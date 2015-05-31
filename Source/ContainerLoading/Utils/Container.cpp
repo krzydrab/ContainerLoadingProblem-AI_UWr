24d7156b5926b66d34968252e233b2bc3074c782
Krabex <krabex93@gmail.com>
2015-05-31 11:40:08 +0200

    Added container class

---
+++ Source/ContainerLoading/Utils/Container.cpp
@@ -0,0 +1,35 @@
+#include "Container.h"
+
+namespace ContainerLoading
+{
+    namespace Utils
+    {
+        Container::Container(const Dimensions dimensions, const glm::float32 layerWidth, const glm::vec3 position)
+        {
+            _dimensions = dimensions;
+            _position = position;
+
+            glm::float32 x = 0;
+            while(x < _dimensions.width)
+            {
+                _layers.push_back(Layer(_dimensions, _position + glm::vec3(x, .0f, .0f)));
+                x += layerWidth;
+            }
+        }
+
+        std::vector<Layer>& Container::getLayers()
+        {
+            return _layers;
+        }
+
+        Dimensions Container::getDimensions() const
+        {
+            return _dimensions;
+        }
+
+        glm::vec3 Container::getPosition() const
+        {
+            return _position;
+        }
+    }
+}
---
+++ Source/ContainerLoading/Utils/Container.h
@@ -0,0 +1,31 @@
+#ifndef CONTAINERLOADING_CONTAINER
+#define CONTAINERLOADING_CONTAINER
+
+#include "Layer.h"
+
+namespace ContainerLoading
+{
+    namespace Utils
+    {
+        class Container
+        {
+        public:
+            Container(
+                const Dimensions dimensions, 
+                const glm::float32 layerWidth, 
+                const glm::vec3 position = glm::vec3(.0f, .0f, .0f)
+            );
+
+            std::vector<Layer>& getLayers();
+            Dimensions getDimensions() const;
+            glm::vec3 getPosition() const;
+
+        private:
+            std::vector<Layer> _layers;
+            Dimensions _dimensions;
+            glm::vec3 _position;
+        };
+    }
+}
+
+#endif /* CONTAINERLOADING_CONTAINER */
